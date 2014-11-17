41d666debc0e5f694fca49d4f0ec0a23ef0b9aba
jveiga <joao.a.veiga@gmail.com>
2014-11-17 00:13:20 +0000

    style: js linting

--- js/gw-ui.select.js
+++ js/gw-ui.select.js
@@ -1,21 +1,22 @@
-(function () {
+(function (angular) {
+  "use strict";
+
   angular.module('gw-ui.select', []).
     directive('gwSelect', [function() {
       return {
-        scope: {gwSelected: '=', gwElements: '=', gwFilter: '='},
+        scope: {gwSelected: '=', gwElements: '=', gwFilter: '=', gwShow: '='},
         restrict: 'AE',
         template: '<div class="btn-group gw-select" ng-style="fixedWidth">' +
             '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">' +
               '<span class="current">{{ gwSelected[label] }}</span> <span class="caret"></span>' +
             '</button>' +
-            '<ul class="dropdown-menu" role="menu">' +
+            '<ul class="dropdown-menu" role="menu" ng-class="{show: gwShow}">' +
               '<li ng-repeat="element in gwElements | filter: gwFilter"><a ng-click="select(element)">{{ element[label] }}</a></li>' +
             '</ul>' +
           '</div>',
         replace: true,
         link: function($scope, iElm, iAttrs) {
           $scope.label = iAttrs.gwLabel;
-          if (iAttrs.gwShow) iElm.find('ul').addClass("show");
           if (parseInt(iAttrs.gwWidth, 10) > 10) {
             iElm.addClass("fixed-width");
             iElm.find('span.current').addClass('pull-left');
@@ -27,4 +28,4 @@
         }
       };
     }]);
-})();
+})(angular);
